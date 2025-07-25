c62a154a9081365f1f6aa45ae1282045702368e6
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-07-25 08:48:16 -0400

    allow for local launch

--- Program.cs
+++ Program.cs
@@ -3,28 +3,38 @@ using Symbiosis;
 using System;
 using System.Net;
 
-Console.Write("Port to use: ");
-var port = Convert.ToInt32(Console.ReadLine());
-Console.Write("Remote IP and port to connect to: ");
-var remoteAddress = Console.ReadLine();
-Console.Write("Connect as player 1 or 2: ");
-var player = Convert.ToInt32(Console.ReadLine());
+var sessionBuilder = RollbackNetcode.WithInputType<PlayerInputs>();
 
-IPEndPoint endPoint;
-IPEndPoint.TryParse(remoteAddress, out endPoint);
+Console.Write("(L)ocal or (R)emote: ");
+var mode = Console.ReadLine();
 
-NetcodePlayer[] players;
-if (player == 1)
-    players = [NetcodePlayer.CreateLocal(), NetcodePlayer.CreateRemote(endPoint)];
-else
-    players = [NetcodePlayer.CreateRemote(endPoint), NetcodePlayer.CreateLocal()];
+if (mode.StartsWith("R")) {
+    Console.Write("Port to use: ");
+    var port = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Remote IP and port to connect to: ");
+    var remoteAddress = Console.ReadLine();
+    Console.Write("Connect as player 1 or 2: ");
+    var player = Convert.ToInt32(Console.ReadLine());
 
-var session = RollbackNetcode
-        .WithInputType<PlayerInputs>()
+    IPEndPoint endPoint;
+    IPEndPoint.TryParse(remoteAddress, out endPoint);
+
+    NetcodePlayer[] players;
+    if (player == 1)
+        players = [NetcodePlayer.CreateLocal(), NetcodePlayer.CreateRemote(endPoint)];
+    else
+        players = [NetcodePlayer.CreateRemote(endPoint), NetcodePlayer.CreateLocal()];
+
+    sessionBuilder
         .WithPort(port)
         .WithPlayers(players)
-        .ForRemote()
-        .Build();
+        .ForRemote();
+}
+else
+{
+    sessionBuilder
+        .ForLocal();
+}
 
-using var game = new Game1(session);
+using var game = new Game1(sessionBuilder.Build());
 game.Run();
