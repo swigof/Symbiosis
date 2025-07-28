cdb90e89b6b66557fa02e38b1e9454fc6e6ed94a
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-07-28 11:27:29 -0400

    add struct serializer for input
    from
    https://github.com/Delta3-Studio/Backdash/blob/f767387bf0310916ecf4979c5f9681cb4e6ba603/src/Backdash/Serialization/Internal/StructBinarySerializer.cs

---
+++ Input/StructBinarySerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Backdash.Network;
+using Backdash.Serialization;
+
+namespace Symbiosis.Input;
+
+sealed class StructBinarySerializer<T> : IBinarySerializer<T> where T : unmanaged
+{
+    public Endianness Endianness => Platform.Endianness;
+
+    static readonly int tSize = Unsafe.SizeOf<T>();
+
+    public int Serialize(in T data, Span<byte> buffer)
+    {
+        MemoryMarshal.Write(buffer, in data);
+        return tSize;
+    }
+
+    public int Deserialize(ReadOnlySpan<byte> data, ref T value)
+    {
+        value = MemoryMarshal.Read<T>(data);
+        return tSize;
+    }
+}
