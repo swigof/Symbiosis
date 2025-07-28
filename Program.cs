e0da2c66bab1de42f0ba24c0ee6e121922efcb8c
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-07-28 13:55:45 -0400

    modify launch for default port connection

--- Program.cs
+++ Program.cs
@@ -7,14 +7,26 @@ using System.Net;
 var inputSerializer = new StructBinarySerializer<PlayerInputs>();
 var sessionBuilder = RollbackNetcode.WithInputType(t => t.Custom(inputSerializer));
 
-Console.Write("(L)ocal or (R)emote: ");
-var mode = Console.ReadLine();
+Console.Write("(L)ocal, (R)emote, or (C)ustom: ");
+var mode = Console.ReadLine().ToUpper();
 
-if (mode.ToUpper().StartsWith("R")) {
-    Console.Write("Port to use: ");
-    var port = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Remote IP and port to connect to: ");
-    var remoteAddress = Console.ReadLine();
+if (mode.StartsWith("C") || mode.StartsWith("R")) 
+{
+    var port = 34345;
+    var remoteAddress = "";
+    if (mode.StartsWith("C"))
+    {
+        Console.Write("Port to use: ");
+        port = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Remote IP and port to connect to: ");
+        remoteAddress = Console.ReadLine();
+    }
+    else
+    {
+        Console.Write("Remote IP to connect to: ");
+        remoteAddress = Console.ReadLine() + ":" + port.ToString();
+    }
+
     Console.Write("Connect as player 1 or 2: ");
     var player = Convert.ToInt32(Console.ReadLine());
 
