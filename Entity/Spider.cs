e0b3ec43e3dd4902b4272c3c74b76b910e643cbf
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-07-30 12:56:07 -0400

    add spider controls

--- Entity/Spider.cs
+++ Entity/Spider.cs
@@ -2,46 +2,108 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Symbiosis.Input;
+using System;
 using static Symbiosis.Collision;
 
 namespace Symbiosis.Entity;
 
+internal enum SpiderMovement : byte
+{
+    None,
+    Going,
+    Returning
+}
+
 public class Spider(bool isLocalPlayer) : IBinarySerializable
 {
     // Game State
-    Vector2 _position = Vector2.Zero;
+    Vector2 _position = _home;
     int _radius = 12;
+    byte _movementByte = 0;
+    SpiderMovement _movement
+    {
+        get => (SpiderMovement)_movementByte;
+        set => _movementByte = (byte)value;
+    }
+    Vector2 _target = Vector2.Zero;
 
     Texture2D _idleTexture = Game1.GameContent.Load<Texture2D>("spider");
+    float _rotation = 0;
+    Vector2 _direction = Vector2.Zero;
+    float _movementDistanceSquared = 0;
     public bool IsLocalPlayer = isLocalPlayer;
     public Circle BoundingCircle { get => new Circle { Center = _position, Radius = _radius }; }
 
+    static readonly Vector2 _home = new Vector2(400, 200);
+    static readonly Vector2 _spriteCenter = new Vector2(16, 16);
+
     public void Update(PlayerInputs inputs)
     {
-        if (inputs.DigitalInputs.HasFlag(DigitalInputs.Up))
-            _position.Y--;
-        if (inputs.DigitalInputs.HasFlag(DigitalInputs.Down))
-            _position.Y++;
-        if (inputs.DigitalInputs.HasFlag(DigitalInputs.Left))
-            _position.X--;
-        if (inputs.DigitalInputs.HasFlag(DigitalInputs.Right))
-            _position.X++;
+        if (_movement == SpiderMovement.None)
+        {
+            if (inputs.DigitalInputs.HasFlag(DigitalInputs.Click))
+            {
+                _movement = SpiderMovement.Going;
+                _target = new Vector2(inputs.CursorPosition.X, inputs.CursorPosition.Y);
+                var movementVector = _target - _position;
+                _movementDistanceSquared = movementVector.LengthSquared();
+                _direction = Vector2.Normalize(movementVector);
+                _rotation = (float)Math.Atan2(_direction.X, -_direction.Y);
+            }
+        }
+
+        if (_movement == SpiderMovement.Going)
+        {
+            _position += _direction * 4;
+
+            if ((_position - _home).LengthSquared() >= _movementDistanceSquared)
+            {
+                _position = _target;
+                _rotation = _rotation + MathHelper.Pi;
+                _movement = SpiderMovement.Returning;
+            }
+        }
+        else if (_movement == SpiderMovement.Returning)
+        {
+            _position -= _direction * 4;
+
+            if ((_position - _target).LengthSquared() >= _movementDistanceSquared)
+            {
+                _movement = SpiderMovement.None;
+                _position = _home;
+                _rotation = 0;
+            }
+        }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(_idleTexture, _position, null, Color.White);
+        spriteBatch.Draw(
+            _idleTexture,
+            _position,
+            null,
+            Color.White,
+            _rotation,
+            _spriteCenter,
+            1,
+            SpriteEffects.None,
+            0
+        );
     }
 
     public void Deserialize(ref readonly BinaryBufferReader reader)
     {
         reader.Read(ref _position);
         reader.Read(ref _radius);
+        reader.Read(ref _movementByte);
+        reader.Read(ref _target);
     }
 
     public void Serialize(ref readonly BinaryBufferWriter writer)
     {
         writer.Write(in _position);
         writer.Write(in _radius);
+        writer.Write(in _movementByte);
+        writer.Write(in _target);
     }
 }
