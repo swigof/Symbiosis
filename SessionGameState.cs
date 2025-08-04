1d127f48407e9ebe3c411e9356e13318089e2bd4
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-08-04 14:02:10 -0400

    make game state usage thread safe

--- SessionGameState.cs
+++ SessionGameState.cs
@@ -4,74 +4,120 @@ using Microsoft.Xna.Framework.Graphics;
 using Symbiosis.Entity;
 using Symbiosis.Input;
 using System;
+using System.Threading;
 
 namespace Symbiosis;
 
 public struct SessionGameState : IBinarySerializable
 {
-    public int FrameNumber = 0;
-    public PlayerInputs[] PreviousInputs = new PlayerInputs[2];
-    public Spider Spider;
-    public Frog Frog;
+    int _frameNumber = 0;
+    PlayerInputs[] _previousInputs = new PlayerInputs[2];
+    Spider _spider;
+    Frog _frog;
+
+    Mutex _stateMutex = new Mutex();
 
     public SessionGameState(bool isLocal, int firstLocalPlayerIndex)
     {
         if (isLocal)
         {
-            Spider = new Spider(true);
-            Frog = new Frog(true);
+            _spider = new Spider(true);
+            _frog = new Frog(true);
         }
         else
         {
             // Default player 1 to spider and 2 to frog for now
-            Spider = new Spider(firstLocalPlayerIndex == 0);
-            Frog = new Frog(firstLocalPlayerIndex == 1);
+            _spider = new Spider(firstLocalPlayerIndex == 0);
+            _frog = new Frog(firstLocalPlayerIndex == 1);
         }
     }
 
     private SessionGameState(SessionGameState toCopy)
     {
-        FrameNumber = toCopy.FrameNumber;
-        Spider = toCopy.Spider;
-        Frog = toCopy.Frog;
-        PreviousInputs[0] = toCopy.PreviousInputs[0];
-        PreviousInputs[1] = toCopy.PreviousInputs[1];
+        _frameNumber = toCopy._frameNumber;
+        _spider = toCopy._spider;
+        _frog = toCopy._frog;
+        _previousInputs[0] = toCopy._previousInputs[0];
+        _previousInputs[1] = toCopy._previousInputs[1];
     }
 
-    public bool IsLocalCursorPlayer() => Spider.IsLocalPlayer;
+    // Not thread protected. Not guaranteed to be state alligned. 
+    public bool IsLocalCursorPlayer() => _spider.IsLocalPlayer;
 
-    public SessionGameState GetCopy() => new SessionGameState(this);
+    public SessionGameState GetCopy() {
+        _stateMutex.WaitOne();
+        try
+        {
+            return new SessionGameState(this);
+        }
+        finally
+        {
+            _stateMutex.ReleaseMutex();
+        }
+    }
 
     public void Update(ReadOnlySpan<SynchronizedInput<PlayerInputs>> inputs)
     {
-        FrameNumber++;
-        Spider.Update(inputs[0].Input);
-        Frog.Update(inputs[1].Input);
-        PreviousInputs[0] = inputs[0];
-        PreviousInputs[1] = inputs[1];
+        _stateMutex.WaitOne();
+        try
+        {
+            _frameNumber++;
+            _spider.Update(inputs[0].Input);
+            _frog.Update(inputs[1].Input);
+            _previousInputs[0] = inputs[0];
+            _previousInputs[1] = inputs[1];
+        }
+        finally 
+        {
+            _stateMutex.ReleaseMutex();
+        }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        Frog.Draw(spriteBatch);
-        Spider.Draw(spriteBatch);
+        _stateMutex.WaitOne();
+        try
+        {
+            _frog.Draw(spriteBatch);
+            _spider.Draw(spriteBatch);
+        }
+        finally
+        {
+            _stateMutex.ReleaseMutex();
+        }
     }
 
     public void Deserialize(ref readonly BinaryBufferReader reader)
     {
-        reader.Read(ref FrameNumber);
-        reader.Read(ref Spider);
-        reader.Read(ref Frog);
-        reader.Read(ref PreviousInputs[0]);
-        reader.Read(ref PreviousInputs[1]);
+        _stateMutex.WaitOne();
+        try
+        {
+            reader.Read(ref _frameNumber);
+            reader.Read(ref _spider);
+            reader.Read(ref _frog);
+            reader.Read(ref _previousInputs[0]);
+            reader.Read(ref _previousInputs[1]);
+        }
+        finally
+        { 
+            _stateMutex.ReleaseMutex(); 
+        }
     }
 
     public void Serialize(ref readonly BinaryBufferWriter writer)
     {
-        writer.Write(in FrameNumber);
-        writer.Write(in Spider);
-        writer.Write(in Frog);
-        writer.Write(in PreviousInputs[0]);
-        writer.Write(in PreviousInputs[1]);
+        _stateMutex.WaitOne();
+        try
+        {
+            writer.Write(in _frameNumber);
+            writer.Write(in _spider);
+            writer.Write(in _frog);
+            writer.Write(in _previousInputs[0]);
+            writer.Write(in _previousInputs[1]);
+        }
+        finally
+        {
+            _stateMutex.ReleaseMutex();
+        }
     }
 }
