6758c0e7dccddb91b544af31387298b484eb638c
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-08-11 20:32:44 -0400

    make egg enemy game state public

--- Entity/EggEnemyCluster.cs
+++ Entity/EggEnemyCluster.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Symbiosis.Entity;
 
-internal struct EggEnemy
+public struct EggEnemy
 {
     public bool Active;
     public Vector2 RelativePosition;
@@ -13,9 +13,9 @@ internal struct EggEnemy
 public struct EggEnemyCluster : IBinarySerializable
 {
     // Game state
-    Vector2 _position = Vector2.Zero;
-    EggEnemy[] _eggEnemies = new EggEnemy[8];
-    public bool Active { get; private set; } = false;
+    public Vector2 Position = Vector2.Zero;
+    public EggEnemy[] EggEnemies = new EggEnemy[8];
+    public bool Active = false;
 
     const int _radius = 8;
     const int _eggEnemyRadius = 2;
@@ -28,28 +28,28 @@ public struct EggEnemyCluster : IBinarySerializable
     public void Init()
     {
         Active = true;
-        for (var i = 0; i < _eggEnemies.Length; i++)
+        for (var i = 0; i < EggEnemies.Length; i++)
         {
-            _eggEnemies[i].Active = true;
-            _eggEnemies[i].RelativePosition.X = Game1.Random.NextFloat() * (_radius - _eggEnemyRadius);
-            _eggEnemies[i].RelativePosition.Y = Game1.Random.NextFloat() * (_radius - _eggEnemyRadius);
+            EggEnemies[i].Active = true;
+            EggEnemies[i].RelativePosition.X = Game1.Random.NextFloat() * (_radius - _eggEnemyRadius);
+            EggEnemies[i].RelativePosition.Y = Game1.Random.NextFloat() * (_radius - _eggEnemyRadius);
         }
     }
 
     public void Update()
     {
-        _position += Vector2.Normalize(Spider.Home - _position) * _speed;
+        Position += Vector2.Normalize(Spider.Home - Position) * _speed;
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        for (var i = 0; i < _eggEnemies.Length; i++)
+        for (var i = 0; i < EggEnemies.Length; i++)
         {
-            if (_eggEnemies[i].Active)
+            if (EggEnemies[i].Active)
             {
                 spriteBatch.Draw(
                     _idleTexture,
-                    _position + _eggEnemies[i].RelativePosition,
+                    Position + EggEnemies[i].RelativePosition,
                     null,
                     Color.Red,
                     0,
@@ -64,21 +64,21 @@ public struct EggEnemyCluster : IBinarySerializable
 
     public void Deserialize(ref readonly BinaryBufferReader reader)
     {
-        reader.Read(ref _position);
-        for (var i = 0; i < _eggEnemies.Length; i++)
+        reader.Read(ref Position);
+        for (var i = 0; i < EggEnemies.Length; i++)
         {
-            reader.Read(ref _eggEnemies[i].Active);
-            reader.Read(ref _eggEnemies[i].RelativePosition);
+            reader.Read(ref EggEnemies[i].Active);
+            reader.Read(ref EggEnemies[i].RelativePosition);
         }
     }
 
     public void Serialize(ref readonly BinaryBufferWriter writer)
     {
-        writer.Write(in _position);
-        for (var i = 0; i < _eggEnemies.Length; i++)
+        writer.Write(in Position);
+        for (var i = 0; i < EggEnemies.Length; i++)
         {
-            writer.Write(in _eggEnemies[i].Active);
-            writer.Write(in _eggEnemies[i].RelativePosition);
+            writer.Write(in EggEnemies[i].Active);
+            writer.Write(in EggEnemies[i].RelativePosition);
         }
     }
 }
