7634753981e7baf6af03720e7c25304cf78c2cad
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-08-12 13:36:19 -0400

    add random position generator to spawner

--- Manager/SpawnManager.cs
+++ Manager/SpawnManager.cs
@@ -1,4 +1,5 @@
-﻿using Symbiosis.Session;
+﻿using Microsoft.Xna.Framework;
+using Symbiosis.Session;
 
 namespace Symbiosis.Manager;
 
@@ -7,5 +8,28 @@ public static class SpawnManager
     public static void Update(GameState gamestate)
     {
         
+    }
+
+    private static Vector2 GetRandomPositionJustOutsideScreen()
+    {
+        const int padding = 16;
+        var width = Game1.ScreenBounds.Width + padding;
+        var height = Game1.ScreenBounds.Height + padding;
+        var perimeterPoint = Game1.Random.NextInt(0, width * 2 + height * 2);
+        if (perimeterPoint < width + height)
+        {
+            if (perimeterPoint < width)
+                return new Vector2(perimeterPoint - padding, -padding);
+            else
+                return new Vector2(width - padding, perimeterPoint - width - padding);
+        }
+        else
+        {
+            perimeterPoint = perimeterPoint - (width + height);
+            if (perimeterPoint < width)
+                return new Vector2(width - perimeterPoint - padding, height - padding);
+            else
+                return new Vector2(-padding, height - (perimeterPoint - width) - padding);
+        }
     }
 }
