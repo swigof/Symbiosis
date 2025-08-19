a47a6c020608c577aeb6d6857698d096ee0eddfb
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-08-19 18:30:12 -0400

    add credits text

--- UI/Menu.cs
+++ UI/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Symbiosis.Input;
@@ -12,12 +13,15 @@ public class Menu
     Button _creditsButton;
     Button _resumeButton;
     Button _backButton;
+    String _creditsText;
 
     static readonly Vector2 _resumeButtonPosition = new(Game1.ResolutionWidth / 2f, Game1.ResolutionHeight * 0.7f);
     static readonly Vector2 _retryButtonPosition = new(Game1.ResolutionWidth / 2f, Game1.ResolutionHeight * 0.8f);
     static readonly Vector2 _creditsButtonPosition = new(Game1.ResolutionWidth / 2f, Game1.ResolutionHeight * 0.9f);
+    static readonly Vector2 _creditsPosition = new(48, 48);
     static readonly Texture2D _pauseOverlay = Game1.GameContent.Load<Texture2D>("pause-overlay");
-    readonly static Color _quarterTransparent = new Color(255, 255, 255, 255 / 4);
+    static readonly Color _quarterTransparent = new Color(255, 255, 255, 255 / 4);
+    static readonly SpriteFont _font = Game1.GameContent.Load<SpriteFont>("PublicPixel");
 
     public Menu(EventHandler<EventArgs> reset, EventHandler<EventArgs> credits, EventHandler<EventArgs> resume)
     {
@@ -25,6 +29,11 @@ public class Menu
         _retryButton = new Button(_retryButtonPosition, "Retry", reset);
         _creditsButton = new Button(_creditsButtonPosition, "Credits", credits);
         _backButton = new Button(_creditsButtonPosition, "Back", credits);
+        var stream = TitleContainer.OpenStream("Content/Assets/credits.txt");
+        using (StreamReader sr = new StreamReader(stream))
+        {
+            _creditsText = sr.ReadToEnd();
+        }
     }
 
     public void Update(PlayerInputs mousePlayerInputs, ref GameState gameState)
@@ -47,6 +56,17 @@ public class Menu
         {
             spriteBatch.Draw(_pauseOverlay, Game1.ScreenBounds, Color.White);
             _backButton.Draw(spriteBatch, gameState.BackButtonState);
+            spriteBatch.DrawString(
+                _font, 
+                _creditsText, 
+                _creditsPosition, 
+                Color.White,
+                0,
+                Vector2.Zero,
+                0.5f,
+                SpriteEffects.None,
+                0
+            );
         }
         else
         {
